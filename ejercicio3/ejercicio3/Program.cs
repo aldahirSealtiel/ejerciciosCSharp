276110197daa3ef583445bc5d1fa3c223f89e9f0
aldahir sealtiel alvarez villegas <alda_dsr@hotmail.com>
2019-07-01 00:05:14 -0500

    string format

---
+++ ejercicio3/ejercicio3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = String.Format("{0} times {1} equals {2}", 2, 5, 2*5);
+            Console.WriteLine(myString);
+            Console.WriteLine("Presiona una tecla para terminar...");
+            Console.ReadKey();
+        }
+    }
+}
